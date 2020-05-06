c3197cac0fe08dba698f6cd50a75303c8bdddcb3
SingyMingy <lin86ops@gmail.com>
2020-05-07 11:41:18 +1200

    Added require variables for oci

--- tf-oci/variable.tf
+++ tf-oci/variable.tf
@@ -15,8 +15,24 @@ variable "vcn_cidr" {
   default = "192.168.0.0/16"
 }
 
+variable "instance_display_name" {
+  default = "first_vm"
+}
 
+variable "subnet_ocids" {
+  type    = "list"
+  default = ["192.168.0.0/17"]
+}
+variable "source_ocid" {
+  default     = "ocid1.image.oc1.ap-sydney-1.aaaaaaaacred4yprbcipoynd3xafrhkqw3uyrsjtx7evc23ued5tmydx55dq"
+  description = "centos7"
+}
+variable "ssh_authorized_keys" {}
 
+variable "block_storage_sizes_in_gbs" {
+  type    = "list"
+  default = [60, 70]
+}
 
 
 
